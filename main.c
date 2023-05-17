bd9e1d16bb6f03169ae2341948c3a54b251c798a
iffatunnisa <127869077+iffatunnisa@users.noreply.github.com>
2023-05-17 21:32:21 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,23 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char * argv[]){
+typedef struct registration
+{
+    char name[20];
+    char job[20];
+    char age[20];
+    char hobby[50];
+    char username[20];
+    char password[20];
+} regis;
 
-    char username[20], password[20], repassword[20];
-    char usernamelog[20], passwordlog[20];
-    char input1;
+// proses registrasi
+int main(int manyArgument, char *argument[]) 
+{
+    //pengecekan kondisi pertama
+    if(manyArgument == 1){
+        regis reg; //membuat variable dari struct
+        printf("\n");     
+        printf("\t\t      ___       __   __         ___ \n");
+        printf("\t\t|  | |__  |    /  ` /  \\  |\\/| |__  \n");
+        printf("\t\t|/\\| |___ |___ \\__, \\__/  |  | |___ \n");
+        printf("\t\t___________________________________\n");
+        printf("\t\t       -Welcome to the Game-        \n");
+        printf("\n");
+        printf("Silahkan registrasi terlebih dahulu sebelum memulai game quiz <3\n");
+        printf("Nama\t\t: "); gets(reg.name);
+        printf("Pekerjaan\t: "); gets(reg.job);
+        printf("Hobby\t\t: "); gets(reg.hobby);
+        printf("Umur\t\t: "); gets(reg.age);
+        printf("\n");
+        printf("\tcreate your account\n");
+        printf("Username\t: "); gets(reg.username);
+        printf("Password\t: "); gets(reg.password);
+        printf("\nCreate account success !\n");
+        printf("lets login and enjoy the game buddy!\n");
+        printf("Note cara login : ./FileProgramUtama Username Password\n\n");
+        
+        FILE *fpw = fopen("database/login.bin", "wb");
 
-    if (argc == 3) {
-        goto login;
-    }
+        char insl[3] = "#";
+        strcat(reg.username, insl); //strcat berfungsi untuk menggabungkan 2 buah string
+        strcat(reg.username, reg.password);
+
+        fwrite(reg.username, sizeof(char), sizeof(reg.username)/sizeof(char), fpw);
+
+        fclose(fpw);
+    } // akhir proses registrasi
     
-    FILE *reg;
-
-    printf("Selamat datang\n");
-    printf("Buat akun(y/n): ");
-    scanf("%s", &input1);
-    getchar();
-
     
     if(input1 == 'y'){
         reg = fopen("login.bin", "wb");  
